d1b04767433c42981a00b71854110d3011f53a59
vkipcheck <yicuw@walkmail.ru>
2016-02-08 22:12:29 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 var config = {
-    'redirect_uri': 'http://m.vk.com',
+    'redirect_uri': 'http://vk.com',
     'mongolab_api_key': 'sJTdPt9aIuqM3bPbw08Zxxm7As--nthE',
     'db_name': 'myaccountsdb'
 };
